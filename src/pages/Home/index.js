447575fa672cbbc554e023d07ce143f28e1c73e3
chaotic <mnrealest161@gmail.com>
2024-10-29 18:52:17 +0700

    layout section-4

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,7 @@
 import ButtonAbout from "../../components/ButtonAbout";
 import ButtonServices from "../../components/ButtonServices";
 import "./section.css";
-import { FaBalanceScale, FaFire, FaIcons } from "react-icons/fa";
+import { FaBalanceScale, FaCrosshairs, FaFire, FaIcons, FaRegHandLizard, FaRegHandshake, FaRoute } from "react-icons/fa";
 
 const Home = () => {
     return (
@@ -72,7 +72,34 @@ const Home = () => {
             </div>
             {/* End Section 3 */}
 
-
+            {/* Section 4 */}
+            <div className="section4">
+                <div className="container">
+                    <div className="section4__wrap">
+                        <div className="section4__title">Our Goal</div>
+                        <div className="section4__desc">The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.
+                            Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.
+                            Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+                            The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc. <FaRegHandLizard />
+                        </div>
+                        <div className="section4__list">
+                            <div className="section4__keyword">
+                                <FaRegHandshake />
+                                <span>cooperative</span>
+                            </div>
+                            <div className="section4__keyword">
+                                <FaCrosshairs />
+                                <span>Usage</span>
+                            </div>
+                            <div className="section4__keyword">
+                                <FaRoute />
+                                <span>rapid</span>
+                            </div>
+                        </div>        
+                    </div>
+                </div>
+            </div>
+            {/* End Section 4 */}
         </>
     )
 }
