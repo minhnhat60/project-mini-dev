865a29816f9cc3d09c72dec43037914b4372bbe4
chaotic <mnrealest161@gmail.com>
2024-10-30 16:07:28 +0700

    layout section 1 (about)

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,8 +1,33 @@
+import "./section.css";
+
 const About = () => {
     
     return (
         <>
-            <h2>Trang giới thiệu</h2>
+            {/* Logo */}
+            <div className="logo-ss">
+                <div className="container">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/2/22/SPACESPEAKERS_Logo.png" alt="Logo" />
+                </div>
+            </div>
+
+            {/* End Logo */}
+
+            {/* Section 1 */}
+            <div className="section1__about">
+                <div className="container">
+                    <div className="section1__about__image">
+                        <img src="https://kenh14cdn.com/203336854389633024/2021/7/15/photo-1-1626355444681457517362.jpg" alt="anh-1"/>
+                        <div className="section1__about__desc">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. 
+                        It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</div>
+                    </div>
+                </div>
+            </div>
+            {/* End Section 1 */}
+
+            {/* Section 2 */}
+            
+            {/* End Section 2 */}
         </>
     )
 }
