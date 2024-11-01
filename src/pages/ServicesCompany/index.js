cc9de72b76c9b8acd2b2e5dbf685ee594b399369
chaotic <mnrealest161@gmail.com>
2024-11-01 23:08:34 +0700

    responsive page Services

--- src/pages/ServicesCompany/index.js
+++ src/pages/ServicesCompany/index.js
@@ -23,7 +23,7 @@ const ServicesCompany = () => {
                 <div className="container">
                     <div className="section1__services__wrap">
                         <div className="section1__services__content">
-                            <div className="section1__services__title">Về chúng tôi</div>
+                            <div className="section1__services__title">VỀ CHÚNG TÔI</div>
                             <div className="section1__services__desc">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
                             This book is a treatise on the theory of ethics, very popular during the Renaissance.
                             </div>
