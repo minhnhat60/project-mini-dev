ba5114867f7315ac67adf81c27406ac0e75afdbd
chaotic <mnrealest161@gmail.com>
2024-11-01 23:13:09 +0700

    layout Contact

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,7 +1,17 @@
+import "./section.css";
+
 const Contact = () => {
     return (
         <>
             <h2>Trang liên hệ</h2>
+
+            <div className="section1__contact">
+                <div className="container">
+                    <div className="section1__image">
+                        <img src="https://scontent.fsgn2-3.fna.fbcdn.net/v/t1.6435-9/118176444_1006381923155486_7743162476794212324_n.png?_nc_cat=107&ccb=1-7&_nc_sid=127cfc&_nc_ohc=ws7acskCslsQ7kNvgGU1SH7&_nc_zt=23&_nc_ht=scontent.fsgn2-3.fna&_nc_gid=AA_F3EAsyRqTEgEMazelduD&oh=00_AYA0xg8_gf-1Wu4UNLi8Win9tVEWiHHzpbU40ktnyhqq4w&oe=674C453A" alt="anh-1"/>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
